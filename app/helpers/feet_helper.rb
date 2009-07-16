da922c34c461f12bd25fddc6c75ded403a1fa59f
Eckhard Rotte <erotte@gmail.com>
2009-07-16 09:31:48 +0200

    WIP, loeschen geht noch nicht

--- app/helpers/feet_helper.rb
+++ app/helpers/feet_helper.rb
@@ -1,2 +1,12 @@
 module FeetHelper
+  def remove_shoe_button(text) 
+    link_to_function image_tag('app/cross.png', :alt => " - "), 
+    "$(this).parents('tr').remove()", :title => text 
+  end
+  
+  def add_shoe_button(name) 
+    link_to_function image_tag('app/add.gif', :alt => "+") do |page| 
+      page.insert_html :before, :insert_article_button_row, :partial => 'shoe_fields', :object => Shoe.new
+    end 
+  end
 end
