7e0c11fc46a97f9e4de8b583d349d43153e05275
bosho <bosho@boshos-computer.local>
2009-08-06 20:17:15 +0200

    rspec funktiert fast

--- app/models/foot.rb
+++ app/models/foot.rb
@@ -6,13 +6,13 @@ class Foot < ActiveRecord::Base
   
   def self.similar_feet another_foot 
     self.find_by_sql "
-    select f.* from feet as f, shoes as a, shoes as b 
-    where a.size = b.size 
-    and a.model = b.model
-    and a.manufacturer = b.manufacturer 
-    and a.foot_id != b.foot_id
-    and b.foot_id = #{another_foot.id}
-    and f.id = a.foot_id;"
+      select f.* from feet a  s f, shoes as a, shoes as b 
+      where a.size = b.size 
+      and a.model = b.model
+      and a.manufacturer = b.manufacturer 
+      and a.foot_id != b.foot_id
+      and b.foot_id = #{another_foot.id}
+      and f.id = a.foot_id;"
   end
   
   def fitting_shoes
--- relations.sql
+++
@@ -1,5 +0,0 @@
-select a.foot_id from shoes as a, shoes as b 
-where a.size = b.size 
-and a.model=b.model
-and a.manufacturer=b.manufacturer 
-and a.foot_id != b.foot_id;
