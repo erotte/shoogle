37472a0c19a39a1caf92811504dcc653f6126ecc
bosho <bosho@boshos-computer.local>
2009-08-06 23:20:47 +0200

    durchschnitt geht

--- spec/model/foot_spec.rb
+++ spec/model/foot_spec.rb
@@ -14,11 +14,26 @@ describe Foot do
   end
   
   it "should find similar feet" do
-    Foot.similar_feet(@arbos_fuss).include?(@eckis_fuss).should be_true
+    Foot.similar_feet(@arbos_fuss).should include(@eckis_fuss)
   end
     
-  it "should find shoe of the second foot" do
-    @arbos_fuss.fitting_shoes.include?(@eckis_camper).should be_true
+  it "should find shoes of the similar feet" do
+    @arbos_fuss.shoes_of_similar_feet.should include(@eckis_camper)
   end
 
+  it "should recommend fitting shoes" do
+    @arbos_fuss.fitting_shoes.select{ |shoe| 
+      shoe.manufacturer == 'Camper' and shoe.model == 'Trabant'}.first.should == @eckis_camper
+  end
+
+  it "should recommend only one shoe-model" do
+    @arbos_fuss.fitting_shoes.select{ |shoe| 
+      shoe.model == 'Adi Racer High' }.size.should == 1
+  end
+  
+  it "should compute average shoe size" do
+    @arbos_fuss.fitting_shoes.select{ |shoe| 
+      shoe.model == 'Adi Racer High' }.first.size.should == 46.5
+  end
+  
 end
