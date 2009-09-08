84b48626ed6582dd14e3e3fc2c8be169428aae21
arbovm <arbovm@gmail.com>
2009-09-08 22:39:56 +0200

    manufacturer scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :manufacturers
+
   map.resources :feet, :has_many => :shoes, :shallow => true
   map.resources :shoes
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
