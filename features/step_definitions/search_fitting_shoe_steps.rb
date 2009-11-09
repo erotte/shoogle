0ccc83f6f83f8c31d0212abc98ce94db4d7e0e66
arbovm <arbovm@gmail.com>
2009-11-09 18:34:02 +0100

    bugfix searched shoe, bei neuer session neue suche

--- features/step_definitions/search_fitting_shoe_steps.rb
+++ features/step_definitions/search_fitting_shoe_steps.rb
@@ -6,18 +6,13 @@ Given /^I am searching a "([^\"]*)" "([^\"]*)"$/ do |manufacturer, model|
 end
 
 Given /^I enter shoe "([^\"]*)" "([^\"]*)" in "([^\"]*)"$/ do |manufacturer, model, size|
-  fill_shoe_inputs manufacturer, model, size
+  fill_in("foot_shoes_attributes__manufacturer", :with => manufacturer)
+  fill_in("foot_shoes_attributes__model", :with => model)
+  fill_in("foot_shoes_attributes__size", :with => size)
 end
 
 
 Given /^I have a "([^\"]*)" "([^\"]*)" in "([^\"]*)"$/ do |manufacturer, model, size|
-  fill_shoe_inputs manufacturer, model, size
+  Given I enter shoe "#{manufacturer}" "#{model}" in "#{size}"
   click_button("foot_submit")
-end
-
-
-def fill_shoe_inputs manufacturer, model, size
-  fill_in("foot_shoes_attributes__manufacturer", :with => manufacturer)
-  fill_in("foot_shoes_attributes__model", :with => model)
-  fill_in("foot_shoes_attributes__size", :with => size)
 end
