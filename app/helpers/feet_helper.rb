18a9484ffc698d2cee6c48794a703f309c4439e0
Eckhard Rotte <erotte@gmail.com>
2010-03-01 00:40:28 +0100

    ein bisschen frontend

--- app/helpers/feet_helper.rb
+++ app/helpers/feet_helper.rb
@@ -1,4 +1,14 @@
 module FeetHelper
 
-  
+  def searched_shoe_form_options
+    { :update => 'add_shoe_form',
+      :complete => '$("#add_shoe_form").show();
+                    init_shoe_completer();
+                    $("#foot_fields input.manufacturer").focus();
+                    $("#losjetzt_eingeben").show("drop", { direction: "up" }) 
+                    $("#search_shoes_submit").val("Ändern")',
+      :html => {:class => "foot_shoes_form", :id => "searched_shoe_form" }
+    }  
+  end
+
 end
