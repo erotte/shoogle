77a472be230b9cc607d466b76bc7897fec4d259b
Eckhard Rotte <erotte@gmail.com>
2010-03-14 13:35:29 +0100

    Deployment geändert

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,17 +7,19 @@
  set :revision,              'master'
  set :repository,            'git@github.com:erotte/shoogle.git'
  set :deploy_to,             '/home/shooser/webapp/alpha'
- set :web_command,            'nginx'
- 
+ set :web_command,           'nginx'
+ set :deploy_via,            :copy
+ set :git_shallow_clone,     1
+
  namespace :vlad do
-   # set :app_command, "/etc/init.d/nginx"
+   set :app_command, "/etc/init.d/nginx"
   
  
    desc "Pull from git, run migrations, then (re)start the app server"
-   task :migrate_deploy => [:update, :migrate, :start_app]
+   task :migrate_deploy => [:update, :migrate, :start_app, :cleanup ]
  
    desc "Pull from git then (re)start the app server"
-   task :deploy => [:update, :start_app]
+   task :deploy => [:update, :start_app, :cleanup]
  
    desc 'Restart Passenger'
    remote_task :restart do
@@ -25,8 +27,8 @@
      run "touch #{deploy_to}/current/tmp/restart.txt"
    end
    
-   # desc 'Restarts the nginx servers'
-   # remote_task :start_web, :roles => :app do
-   #   run "sudo #{app_command} restart"
-   # end
+   desc 'Restarts the nginx servers'
+   remote_task :start_web, :roles => :app do
+     run "sudo #{app_command} restart"
+   end
  end
