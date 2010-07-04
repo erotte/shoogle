12fef5c6c6dd994df73967fd704a5665be7ef6e8
Eckhard Rotte <erotte@gmail.com>
2010-07-04 11:43:44 +0200

    Rails 2.3.8 update

--- app/models/searched_shoe.rb
+++ app/models/searched_shoe.rb
@@ -4,6 +4,7 @@ class SearchedShoe
   property :model_name
   property :manufacturer_name
   
-  validates_presence_of :model_name
+  validates_presence_of :manufacturer_name
+  
 
 end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -9,9 +9,9 @@ class Shoe
 #  before_save :adjust_size_type
   validates_presence_of :manufacturer, :message => "Bitte gib einen Hersteller ein"
   validates_presence_of :model, :message => "Bitte gib ein Schuhmodell ein"
-  validates_presence_of :size, :message => "Bitte gib eine Größe ein" , :message => "Bitte gib eine Größe ein"
+  validates_presence_of :size, :message => "Bitte gib eine Größe ein" 
   # validates_numericality_of :size, :message => "Bitte" 
-
+  
 
   def adjust_size_type
     if size.to_i.between?(5, 15)
