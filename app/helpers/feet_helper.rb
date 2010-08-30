82a79deec9b55205b08a22b0f24acdca6fd16d87
Eckhard Rotte <erotte@gmail.com>
2010-08-30 17:14:03 +0200

    Tipsy-Tooltips (Github-like) Bug: Schliessen nicht zuverlaässig nach Ajax Update

--- app/helpers/feet_helper.rb
+++ app/helpers/feet_helper.rb
@@ -13,20 +13,31 @@ module FeetHelper
     }  
   end
 
-  def delete_seached_shoe_button
-    link_to_remote image_tag('app/icons_small/cross.png' , :class => 'delete'),
-                   :url => foot_searched_shoes_path(@foot),
-                   :method => :delete, :update => 'panel'
+  def delete_seached_shoe_button opts={}
+    link_to_remote image_tag('app/icons_small/cross.png' ),
+                   {
+                    :url => foot_searched_shoes_path(@foot),
+                    :html => {:title  => "löschen", :class => 'delete', :rel => 'tipsy'},
+                    :method => :delete, :update => 'panel'}.merge(opts)
   end
 
-  def edit_seached_shoe_button
-     link_to_remote image_tag('app/icons_small/edit.png' , :class => 'edit'),
-                    :url => edit_foot_searched_shoes_path(@foot),
+  def edit_seached_shoe_button opts={}
+     link_to_remote image_tag('app/icons_small/edit.png' ),
+                    {:url => edit_foot_searched_shoes_path(@foot),
                     :method => :get, 
                     :update => 'searched_shoe',
-                    :complete => "$('#searched_shoe_form').shoe_completer()"  
+                    :html => {:title => "bearbeiten", :rel => 'tipsy', :class => 'edit'},        
+                    :complete => "$('#searched_shoe_form').shoe_completer()"}.merge(opts)  
   end
-  
+
+  def delete_shoe_button counter
+    link_to_remote image_tag('app/icons_small/cross.png'),
+                   :url => "/feet/#{@foot.id}/shoes/#{counter}",
+                   :method => :delete, :update => 'shoes_list',
+                   :html => {:class => 'delete', :title => 'Schuh löschen', :rel => 'tipsy'}
+
+  end
+
   def add_shoes_options
     { :update => 'shoe_area',
       :html => {:class => 'foot_shoes_form', :id => 'foot_fields'},
